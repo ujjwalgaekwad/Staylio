78f7b01b1385b6b81127cabe9c7dd02ee29f9fb3
Ujjwal <ujjwalsinghgaekwad@gmail.com>
2025-04-27 01:31:18 +0530

    add cross-env for environment variable management and update .gitignore

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,7 @@ import { connectDB } from "./db";
 import registerRouter from "./routes/user.routes";
 import authRouter from "./routes/auth.routes";
 
-connectDB();
+connectDB()
 
 const app = express();
 
@@ -18,7 +18,7 @@ app.use(
 );
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", authRouter);
 app.use("/api/user", registerRouter);
 
