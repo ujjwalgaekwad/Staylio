80b5c279e7348a31ead07d16e8c63fe910f5be98
Ujjwal <ujjwalsinghgaekwad@gmail.com>
2025-05-17 00:17:31 +0530

    clean up index.ts by removing unused imports and commented-out code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 import { connectDB } from "./db";
 import registerRouter from "./routes/user.routes";
 import authRouter from "./routes/auth.routes";
-import path, { join } from "path";
+import path from "path";
 import { v2 as cloudinary } from "cloudinary";
 import myhotels from "../src/routes/myHotels.routes";
 import searchRoutes from "../src/routes/hotel.routes";
@@ -35,15 +35,7 @@ app.use("/api/auth", authRouter);
 app.use("/api/user", registerRouter);
 app.use("/api/hotels", myhotels);
 app.use("/api/hotelSearch", searchRoutes);
-// app.get("*", (req: Request, res: Response) => {
-//   res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
-// })
 
-app.get("/", (req, res) => {
-  console.log("Home request:", req);
-  console.log("home response:", res);
-  console.log("Request Query:", req.query);
-});
 
 app.listen(process.env.PORT, () => {
   console.log(`server is running at:${process.env.PORT}`);
