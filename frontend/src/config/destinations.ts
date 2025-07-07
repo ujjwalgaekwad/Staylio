75865afb75a99296393883560f869438b4e9d6ff
Ujjwal <ujjwalsinghgaekwad@gmail.com>
2025-07-08 00:25:48 +0530

    : Add new destination images and update image paths in destinations configuration

--- frontend/src/config/destinations.ts
+++ frontend/src/config/destinations.ts
@@ -2,37 +2,31 @@ export const destinations = [
   {
     id: 1,
     name: "River Front",
-    image:
-      "https://images.unsplash.com/photo-1638006524490-492fdf36ee04?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YWhtZWRhYmFkfGVufDB8fDB8fHww",
+    image: "./AHM-1.avif",
   },
   {
     id: 2,
     name: "Akshardham Temple",
-    image:
-      "https://utsav.gov.in/public/uploads/event_picture_image/event_1047/16628788661297468980.jpg",
+    image: "./Akshardham-temple.jpg",
   },
   {
     id: 3,
     name: "Adalaj Stepwell",
-    image:
-      "https://theheritageart.com/wp-content/uploads/2022/12/1545473329_adalaj-stepwell1-1000x565.jpeg",
+    image:"./adalaj-stepwell.jpg",
   },
   {
     id: 4,
     name: "Kankaria Lake",
-    image:
-      "https://indiano.travel/wp-content/uploads/2022/04/People-sitting-on-the-circular-area-around-kankaria-lake-in-Ahmedabad.jpg",
+    image:"./kankaria-lake.jpg",
   },
   {
     id: 5,
     name: "Iskcon Temple",
-    image:
-      "https://files.yappe.in/place/full/iskcon-ahmedabad-sri-sri-radha-govindji-temple-102137.webp",
+    image: "./iskcon.webp",
   },
   {
     id: 6,
     name: "Sabarmati Ashram",
-    image:
-      "https://img.traveltriangle.com/blog/wp-content/uploads/2019/06/Sabarmati-Ashram-cover.jpg",
+    image: "./Sabarmati-Ashram.webp"
   },
 ];
